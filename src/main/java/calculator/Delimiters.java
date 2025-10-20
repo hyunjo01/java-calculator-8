76fe617a0453696d87148221b35bf7e66de223b8
hyunjo <hyunjo10217@gmail.com>
2025-10-20 17:31:36 +0900

    feat(parser): 커스텀 구분자 예외 처리

--- src/main/java/calculator/Delimiters.java
+++ src/main/java/calculator/Delimiters.java
@@ -6,6 +6,14 @@ public class Delimiters {
 
     private static final String defaultDelimiter1 = ",";
     private static final String defaultDelimiter2 = ":";
+    private static final int DELIMITER_LENGTH = 1;
+    private static final String NUMBER_REGEX = "\\d+";
+
+    private static final String CUSTOM_DELIMITER_LENGTH_ERROR_MESSAGE = "커스텀 구분자는 한글자로 입력하세요.";
+    private static final String CUSTOM_DELIMITER_DUPLICATION_ERROR_MESSAGE = "입력하신 커스텀 구분자가 기본 구분자와 중복입니다.";
+    private static final String CUSTOM_DELIMITER_NUMBER_ERROR_MESSAGE = "숫자는 구분자로 사용이 불가합니다.";
+
+
     private ArrayList<String> values;
 
     public Delimiters() {
@@ -20,7 +28,9 @@ public class Delimiters {
     }
 
     private void validateDelimiter(String delimiter) {
-
+        if (delimiter.length() != DELIMITER_LENGTH) throw new IllegalArgumentException(CUSTOM_DELIMITER_LENGTH_ERROR_MESSAGE);
+        if (values.contains(delimiter)) throw new IllegalArgumentException(CUSTOM_DELIMITER_DUPLICATION_ERROR_MESSAGE);
+        if (delimiter.matches(NUMBER_REGEX)) throw new IllegalArgumentException(CUSTOM_DELIMITER_NUMBER_ERROR_MESSAGE);
     }
 
     @Override
