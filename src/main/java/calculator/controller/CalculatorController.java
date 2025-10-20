aaf7de126c91195065719a5917685d7b0cd765e9
hyunjo <hyunjo10217@gmail.com>
2025-10-20 10:26:42 +0900

    feat(controller): 컨트롤러 생성

---
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -0,0 +1,17 @@
+package calculator.controller;
+
+import calculator.view.InputView;
+
+public class CalculatorController {
+
+    private final InputView inputView;
+
+    public CalculatorController(InputView inputView) {
+        this.inputView = inputView;
+    }
+
+    public void run() {
+        String inputString = inputView.view();
+    }
+
+}
