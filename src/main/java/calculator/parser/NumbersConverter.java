906e06e7fc592b279e9a79fb19f26de86a01c77e
hyunjo <hyunjo10217@gmail.com>
2025-10-20 15:40:33 +0900

    style(parser): NumbersConverter.class 상수명 오타 수정

--- src/main/java/calculator/parser/NumbersConverter.java
+++ src/main/java/calculator/parser/NumbersConverter.java
@@ -4,7 +4,7 @@ import calculator.Numbers;
 
 public class NumbersConverter {
 
-    private static final String OPERAND_EMPYT_REGEX = "";
+    private static final String OPERAND_EMPTY_REGEX = "";
     private static final String OPERAND_NUMBER_REGEX = "\\d+";
     private static final String OPERAND_NOT_NUMBER_ERROR_MESSAGE = "피연산자에 숫자가 아닌 값이 포함되었습니다.";
     private static final String OPERAND_OUT_OF_RANGE_ERROR_MESSAGE = "피연산자의 수가 너무 큽니다.";
@@ -12,7 +12,7 @@ public class NumbersConverter {
     public Numbers convertToNumbers(String[] tokens) {
         Numbers numbers = new Numbers();
         for (String token: tokens) {
-            if (token.matches(OPERAND_EMPYT_REGEX)) continue;
+            if (token.matches(OPERAND_EMPTY_REGEX)) continue;
             numbers.addOperand(covert(token));
         }
         return numbers;
