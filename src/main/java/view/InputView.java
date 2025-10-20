4135659530070af4474984b893154b44ef498e3e
hyunjo <hyunjo10217@gmail.com>
2025-10-20 09:50:59 +0900

    feat: 사용자 입력 받기

---
+++ src/main/java/view/InputView.java
@@ -0,0 +1,15 @@
+package view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    public InputView() {
+
+    }
+
+    public String view() {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        return Console.readLine();
+    }
+}
